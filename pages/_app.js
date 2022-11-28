fcaeb1924c88b6f7608d057eeed9dfcccfff53f7
uzunhair <uzunhair@yandex.ru>
2022-11-28 09:40:53 +0300

    Fix sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import '../styles/globals.css'
-import '../styles/scss/global.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
