e75e30ec9488dd22e16a95e6d82743a20d9e80d6
uzunhair <uzunhair@yandex.ru>
2022-12-01 12:08:46 +0300

    Add font Manrope

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import { Manrope } from '@next/font/google';
+
+const manrope = Manrope({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        body {
+          font-family: ${manrope.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
